7ba72d92878665ae63b533c5e677c4e6d6f70e26
Misha Wagner <mishajw@gmail.com>
2016-04-16 15:14:18 +0000

    Builds play differently for minimising javascript

--- build-scripts/build-play.sh
+++ build-scripts/build-play.sh
@@ -2,6 +2,9 @@
 
 PROJECT_PATH="$1"
 PROJECT_NAME="$2"
+SYSTEMD_NAME="$3"
+
+sudo systemctl stop $SYSTEMD_NAME
 
 echo Go to the project
 cd $PROJECT_PATH
@@ -10,6 +13,7 @@ echo Getting the latest version
 git pull origin master
 
 echo Build the project
+activator stage
 activator dist
 
 echo Go to the built project
@@ -20,4 +24,6 @@ rm -rf $(echo $PROJECT_NAME)/
 
 echo Unzip the new version
 unzip $(echo $PROJECT_NAME).zip
+
+sudo systemctl start $SYSTEMD_NAME
 
